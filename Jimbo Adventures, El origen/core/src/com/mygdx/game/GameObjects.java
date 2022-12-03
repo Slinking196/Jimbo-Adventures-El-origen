9411d75d3f660dba731d5d7cf666b9e733e885e6
Slinking196 <93099735+Slinking196@users.noreply.github.com>
2022-12-03 05:37:15 -0300

    Delete Jimbo Adventures, El origen directory

--- Jimbo Adventures, El origen/core/src/com/mygdx/game/GameObjects.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.game;
-
-import com.badlogic.gdx.physics.box2d.World;
-
-public abstract class GameObjects implements Dibujable{
-	
-	public abstract void createBody(float x, float y, World world);
-	
-}
